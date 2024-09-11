d0801ebec08852a679122f61d634c3487359cba8
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-09-11 16:01:04 -0500

    Cosas del Paredes

---
+++ Carpeta/Aplicaciones.php
@@ -0,0 +1,12 @@
+<?php require_once ('header_menu_sectionLogo.php'); ?>
+<?php require_once ('header_menu.php'); ?>
+
+<div class="page-wrapper d-flex justify-content-center align-items-center">
+    <a href="Random-Number.html">Generador de numeros aleatorios</a>
+    <a href="Crono.php">Hora actual</a>
+    <a href="Hex.php">Conversor de decimal a hexadecimal</a>
+    <a href="IBM-Calculator.html">Calculador de peso corporal</a>
+    <a href="Binary.php">Conversor de decimal a binario</a>
+</div>
+
+<?php require_once ('footer.php'); ?>
---
+++ Carpeta/Crono.php
@@ -0,0 +1,12 @@
+<?php require_once('header_menu_sectionLogo.php'); ?>
+<?php require_once('header_menu.php'); ?>
+
+<div class="page-wrapper d-flex justify-content-center align-items-center">
+    <div class="container container-tight py-4">
+        <script src="https://cdn.logwork.com/widget/clock.js"></script>
+        <a href="https://logwork.com/current-time-in-ambato-ecuador-tungurahua" class="clock-time"
+            data-style="default-numeral" data-size="250">Current time in Ambato, Ecuador</a>
+    </div>
+</div>
+
+<?php require_once('footer.php'); ?>

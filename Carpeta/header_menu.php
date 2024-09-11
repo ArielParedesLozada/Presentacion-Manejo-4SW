803b106bc5b77bd1802dba0a98a7ae9f3a9cea88
Josué García <josuegarcab2@hotmail.com>
2024-09-11 15:22:52 -0500

    The page our team was added

--- Carpeta/header_menu.php
+++ Carpeta/header_menu.php
@@ -5,26 +5,7 @@
         <!-- Parte inferior -->
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="register.php">
-              <span
-                class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
-                  stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                  <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
-                  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
-                  <path d="M15 15l3.35 3.35"></path>
-                  <path d="M9 15l-3.35 3.35"></path>
-                  <path d="M5.65 5.65l3.35 3.35"></path>
-                  <path d="M18.35 5.65l-3.35 3.35"></path>
-                </svg>
-              </span>
-              <span class="nav-link-title">Register</span>
-            </a>
-          </li>
-
-          <li class="nav-item active">
-            <a class="nav-link" href="login.php">
+            <a class="nav-link" href="index.php">
               <span
                 class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
@@ -38,11 +19,11 @@
                   <path d="M10 14a3.5 3.5 0 0 0 4 0"></path>
                 </svg>
               </span>
-              <span class="nav-link-title">Login</span>
+              <span class="nav-link-title">Inicio</span>
             </a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link" href="discover">
+            <a class="nav-link" href="our_team.php">
               <span
                 class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
@@ -53,7 +34,22 @@
                   </path>
                 </svg>
               </span>
-              <span class="nav-link-title">Discover</span>
+              <span class="nav-link-title">Our team</span>
+            </a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="applications.php">
+              <span
+                class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                  stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path
+                    d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z">
+                  </path>
+                </svg>
+              </span>
+              <span class="nav-link-title">Applications</span>
             </a>
           </li>
         </ul>
